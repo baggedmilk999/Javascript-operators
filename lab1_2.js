74a7d102900e5c8f0845f6f73c2a6eba042c05ec
baggedmilk999 <89410031+baggedmilk999@users.noreply.github.com>
2021-11-19 13:12:47 -0500

    Add files via upload

---
+++ lab1_2.js
@@ -0,0 +1,22 @@
+var numberOfApples = 10;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+numberOfApples++;
+document.writeln("The number of apples I have is " + numberOfApples + "<br>");
+
+document.write("<br><br>")
+
+var numberOfNinja = 100;
+document.writeln("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.writeln("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.writeln("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.writeln("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.writeln("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.writeln("Created by Jassmine Figueroa");
